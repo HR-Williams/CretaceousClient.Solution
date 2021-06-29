a246ebe50a8b108b550275432fbe84c5c61222b2
HR Williams <halrubinwilliams@gmail.com>
2021-06-29 16:25:37 -0700

    Give models full CRUD capability

--- CretaceousClient/Controllers/AnimalsController.cs
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -1,7 +1,19 @@
-// ...
-// public IActionResult Index()
-// {
-//   var allAnimals = Animal.GetAnimals();
-//   return View(allAnimals);
-// }
-// ...
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CretaceousClient.Models;
+
+namespace CretaceousClient.Controllers
+{
+  public class AnimalsController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allAnimals = Animal.GetAnimals();
+      return View(allAnimals);
+    }
+  }
+} 
